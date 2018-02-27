f63acec3ee8bfa59a5e1bca5b00e0400e60c44c6
Ben Schacht <Ben@2018BenSchacht.lan>
2018-02-27 13:54:34 -0500

    Connect 4lokos to my last braincells

--- script.js
+++ script.js
@@ -1,61 +1,150 @@
 const grid = [
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
 ]
 var column1 = 5
 
 
-var currentPlayer
-var palyerOne
-var playerTwo
+var playerOne = 1
+var playerTwo = 2
+var currentPlayer = playerOne
 
-var redDisc
-var blackDisc
+const destination = document.getElementById('container')
 
-for (let i=0; i < grid.length; i++) {
-    let boxColumn  = grid[i]
-    boxColumn = document.createElement('div')
-    boxColumn.className = 'columns'
-    boxColumn.id = 'column' + i
-    let destination = document.getElementById('container')
-    destination.appendChild(boxColumn)
-    boxColumn.addEventListener('click', addPiece);
-    // for (var o = 0; o < column.length; o++) {
-    //     var row = column[o];
-    //     var boxRow = document.createElement('div')
-    //     boxRow.id ='row' + o
-    //     boxRow.className = 'boxes'
-    //     let destination = document.getElementById('column' + i)
-    //     destination.appendChild(boxRow);
-    // }
-};
+function drawGrid() {
+    while (destination.lastChild) {
+        destination.removeChild(destination.lastChild)
+    }
+
+    for (let i = 0; i < grid.length; i++) {
+        let column = grid[i]
+        let boxColumn = document.createElement('div')
+        boxColumn.className = 'columns'
+        boxColumn.id = 'column' + i
+        destination.appendChild(boxColumn)
+        if (column.length <= 5) {
+            boxColumn.addEventListener('click', addPiece);
+        }
+        for (let j = 0; j < column.length; j++) {
+            var row = column[j];
+            if (row == 1) {
+                drawPiece(boxColumn, "red")
+            } else if (row == 2) {
+                drawPiece(boxColumn, "black")
+            }
+
+        }
+    }
+}
+function drawPiece(column, color) {
+    let circle = document.createElement('div')
+    circle.classList.add('circle', color)
+    column.appendChild(circle);
+}
 
 function addPiece(event) {
-    // console.log(event.target);
-    // console.log(event.target.parentNode)
-    let circle = document.createElement('div')
-    circle.className = 'circle'
-    let destination = event.target;
-    destination.appendChild(circle);   
-    console.log(event.target.class)
+    let boxColumn = event.currentTarget
+    var columnID = boxColumn.id.split('column')[1];
+    grid[columnID].push(currentPlayer);
+    drawGrid();
+    checkWin();
+    switchPlayer();
 }
 
-function addToArray (event) {
-    
+function switchPlayer() {
+    if (currentPlayer === playerOne) {
+        currentPlayer = playerTwo;
+    } else if (currentPlayer === playerTwo) {
+        currentPlayer = playerOne;
+    }
+}
+function checkWin() {
+    checkVertical();
+    checkHorizontal();
+    checkDiagonal();
 }
 
-// function switchPlayer () {
-//     if (currentPlayer = playerOne) {
-//         currentPlayer = playerTwo;
-//     } else if (currentPlayer = playerTwo) {
-//         currentPlayer = palyerOne;
-//     }
+function checkVertical() {
+    let playerOneWins = [1, 1, 1, 1].toString();
+    let playerTwoWins = [2, 2, 2, 2].toString();
+    for (let k = 0; k < grid.length; k++) {
+        for (let p = 0; p < 7; p++)
+            if (grid[k][p] == currentPlayer) {
+                let neighbors = grid[k].slice(p, p + 4).toString();
+                // console.log(currentPlayer, neighbors, playerOneWins, playerTwoWins)
+                if (neighbors == playerOneWins) {
+                    alert('Player 1 Wins')
+                } else if (neighbors == playerTwoWins) {
+                    alert("Player 2 Wins")
+                }
 
-// }
 
 
+            }
+    }
+}
+
+function checkHorizontal() {
+    let playerOneWins = [1, 1, 1, 1].toString();
+    let playerTwoWins = [2, 2, 2, 2].toString();
+    for (let k = 0; k < grid.length; k++) {
+        for (let p = 0; p < 7; p++)
+            if (grid[k][p] == currentPlayer && k <= grid.length - 4) {
+                let neighbors = [
+                    grid[k][p],
+                    grid[k + 1][p],
+                    grid[k + 2][p],
+                    grid[k + 3][p],
+                ].toString();
+                // console.log(currentPlayer, neighbors, playerOneWins, playerTwoWins)
+                if (neighbors == playerOneWins) {
+                    alert('Player 1 Wins')
+                } else if (neighbors == playerTwoWins) {
+                    alert("Player 2 Wins")
+                }
+
+
+
+            }
+    }
+}
+
+function checkDiagonal() {
+    let playerOneWins = [1, 1, 1, 1].toString();
+    let playerTwoWins = [2, 2, 2, 2].toString();
+    for (let k = 0; k < grid.length; k++) {
+        for (let p = 0; p < 7; p++)
+            if (grid[k][p] == currentPlayer && k <= grid.length - 4) {
+                let neighbors = [
+                    grid[k][p],
+                    grid[k + 1][p + 1],
+                    grid[k + 2][p + 2],
+                    grid[k + 3][p + 3],
+                ].toString();
+                let negNeighbors = [
+                    grid[k][p],
+                    grid[k + 1][p - 1],
+                    grid[k + 2][p - 2],
+                    grid[k + 3][p - 3],
+                ].toString();
+                console.log(neighbors)
+                if (neighbors == playerOneWins || negNeighbors == playerOneWins) {
+                    alert('Player 1 Wins')
+                } else if (neighbors == playerTwoWins || negNeighbors == playerTwoWins) {
+                    alert("Player 2 Wins")
+                }
+
+
+
+            }
+    }
+}
+
+
+
+drawGrid();
